5ca6c6c175dc583a2cfcbee7a6adeb02838adaeb
dokdzym <44903758+dokdzym@users.noreply.github.com>
2020-03-09 19:43:51 +0800

    Update http_parse.h

--- Source_Code/http_parse.h
+++ Source_Code/http_parse.h
@@ -2,7 +2,15 @@
 // Created by jojo on 2020/3/9.
 //
 
-#ifndef UNTITLED_HTTP_PARSE_H
-#define UNTITLED_HTTP_PARSE_H
+#ifndef HTTP_PARSE_H
+#define HTTP_PARSE_H
 
-#endif //UNTITLED_HTTP_PARSE_H
+#define CR '\r'
+#define LF '\n'
+
+// http请求行解析
+int jo_http_parse_request_line(jo_http_request_t *request);
+// http请求体解析
+int jo_http_parse_request_body(jo_http_request_t *request);
+
+#endif
