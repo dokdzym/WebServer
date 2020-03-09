a53053cfe5b678b65528a7c51135ed7e592a795c
dokdzym <44903758+dokdzym@users.noreply.github.com>
2020-03-09 19:51:20 +0800

    Update main.cpp

--- Source_Code/main.cpp
+++ Source_Code/main.cpp
@@ -1,6 +1,55 @@
+// 系统头文件
 #include <iostream>
+#include <stdio.h>
+#include "threadpool.h"
+#include "http.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#define DEFAULT_CONFIG "jo.conf"
+
+extern struct epoll_event *events;
+char *conf_file = DEFAULT_CONFIG;
+jo_conf_t conf;
+
+int main(int argc, char *argv[]){
+    // 读取配置文件
+    read_conf(conf_file, &conf);
+
+    // 处理SIGPIPE
+    handle_for_sigpipe();
+
+    // 初始化套接字开始监听
+    int listen_fd = socket_bind_listen(conf.port);
+
+    // 设置为socket非阻塞
+    int rc = make_socket_non_blocking(listen_fd);
+
+    // 创建epoll并注册监听描述符
+    int epoll_fd =jo_epoll_create(0);
+    jo_http_request_t* request = (jo_http_request_t*)malloc(sizeof(jo_http_request_t));
+    jo_init_request_t(request, listen_fd, epoll_fd, conf.root);
+    jo_epoll_add(epoll_fd, listen_fd, request, (EPOLLIN | EPOLLET));
+
+    // 初始化线程池
+    jo_threadpool_t *tp = threadpool_init(conf.thread_num);
+
+    // 初始化计时器
+    jo_timer_init();
+
+    while(1){
+        // 得到最近且未删除时间和当前时间差值（等待时间）
+        int time = jo_find_timer();
+
+        // 调用epoll_wait函数，返回接收到事件的数量
+        int events_num = jo_epoll_wait(epoll_fd, events, MAXEVENTS, -1);
+
+        // 处理已经超时的请求
+        jo_handle_expire_timers();
+
+        // 遍历events数组，根据监听种类及描述符类型分发操作
+        jo_handle_events(epoll_fd, listen_fd, events, events_num, conf.root, tp);
+    }
+
+    // 回收线程资源
+    // threadpool_destroy(tp, graceful_shutdown);
 }
+
