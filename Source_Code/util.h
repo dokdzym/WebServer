c8cb76aae9a8851577ffc63aa8acc3d8c128a608
dokdzym <44903758+dokdzym@users.noreply.github.com>
2020-03-09 20:08:33 +0800

    Update util.h

--- Source_Code/util.h
+++ Source_Code/util.h
@@ -2,7 +2,29 @@
 // Created by jojo on 2020/3/9.
 //
 
-#ifndef UNTITLED_UTIL_H
-#define UNTITLED_UTIL_H
+#ifndef UTIL_H
+#define UTIL_H
 
-#endif //UNTITLED_UTIL_H
+#define PATHLEN 128
+#define LISTENQ 1024
+#define BUFLEN 8192
+#define DELIM "="
+
+#define JO_CONF_OK 0
+#define JO_CONF_ERROR -1
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+typedef struct jo_conf{
+    char root[PATHLEN];
+    int port;
+    int thread_num;
+}jo_conf_t;
+
+int read_conf(char* filename, jo_conf_t* conf);
+void handle_for_sigpipe();
+int socket_bind_listen(int port);
+int make_socket_non_blocking(int fd);
+void accept_connection(int listen_fd, int epoll_fd, char* path);
+
+#endif
